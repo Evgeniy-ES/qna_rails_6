f0226be488079e533bc60dc98083986534090c9b
Evgeniy-ES <ees86@yandex.ru>
2022-04-11 14:27:10 +0300

    Attempt 1.1

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,6 +6,7 @@ class Question < ApplicationRecord
   has_many_attached :files
 
   accepts_nested_attributes_for :links, reject_if: :all_blank
+  accepts_nested_attributes_for :reward, reject_if: :all_blank
 
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :best_answer, class_name: 'Answer', optional: true
