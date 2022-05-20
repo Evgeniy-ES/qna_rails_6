ba596307a6cee6e434bc818b76a80e2d8878999e
Evgeniy-ES <ees86@yandex.ru>
2022-05-20 16:20:32 +0300

    attempt 1

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -20,22 +20,57 @@ describe Ability, type: :model do
   end
 
   describe 'for user' do
-   let(:user) { create :user }
-   let(:other) { create :user }
+    let(:user) { create :user }
+    let(:other) { create :user }
+    let(:question) { create(:question, author: user) }
+    let(:other_question) { create(:question, author: other) }
+    let(:answer) { create(:answer, question: question, author: user) }
+    let(:other_answer) { create(:answer, question: other_question, author: other) }
 
+    it { should_not be_able_to :manage, :all }
+    it { should be_able_to :read, :all }
 
-   it { should_not be_able_to :manage, :all }
-   it { should be_able_to :read, :all }
+    context 'create' do
+      it { should be_able_to :create, Question }
+      it { should be_able_to :create, Answer }
+      it { should be_able_to :create, Comment }
+    end
 
-   it { should be_able_to :create, Question }
-   it { should be_able_to :create, Answer }
-   it { should be_able_to :create, Comment }
+    context 'update' do
+      it { should be_able_to :update, question }
+      it { should_not be_able_to :update, other_question }
+      it { should be_able_to :update, answer }
+      it { should_not be_able_to :update, other_answer }
+    end
 
-   it { should be_able_to :update, create(:question, user: user) }
-   it { should_not be_able_to :update, create(:question, user: other) }
+    context 'comment' do
+      it { should be_able_to :create_comment, Question }
+      it { should be_able_to :create_comment, Answer }
+    end
 
-   it { should be_able_to :update, create(:answer, user: user) }
-   it { should_not be_able_to :update, create(:answer, user: other) }
+    context 'destroy' do
+      it { should be_able_to :destroy, Question }
+      it { should be_able_to :destroy, Answer }
+
+      it { should be_able_to :destroy, create(:link, linkable: question) }
+      it { should_not be_able_to :destroy, create(:link, linkable: other_question) }
+      it { should be_able_to :destroy, create(:link, linkable: answer) }
+      it { should_not be_able_to :destroy, create(:link, linkable: other_answer) }
+    end
+
+    context 'vote' do
+      it { should_not be_able_to [:vote_for, :vote_against, :cancel_voting], question }
+      it { should be_able_to [:vote_for, :vote_against, :cancel_voting], other_question }
+      it { should_not be_able_to [:vote_for, :vote_against, :cancel_voting], answer }
+      it { should be_able_to [:vote_for, :vote_against, :cancel_voting], other_answer }
+    end
+
+    context 'mark as best' do
+      it { should be_able_to :mark_as_best, answer }
+      it { should_not be_able_to :mark_as_best, create(:answer, question: other_question, author: other) }
+    end
+
+    it { should be_able_to :destroy, ActiveStorage::Attachment }
  end
 
 end
