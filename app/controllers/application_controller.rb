735d16d567da44c1add1fe663f152fcb8c72f6d5
Evgeniy-ES <ees86@yandex.ru>
2022-05-23 11:11:30 +0300

    attempt 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  #include Pundit
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
-  end
-
-  #check_authorization
+   respond_to do |format|
+     format.html { redirect_to root_url, alert: exception.message }
+     format.js { render status: :forbidden }
+     format.json { render json: exception.message, status: :forbidden }
+   end
+ end
 end
